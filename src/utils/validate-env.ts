a02c754b256addfd56a95b9859b31369f5bb4b34
Aryan Swaminath <aryan.swaminath@aol.com>
2024-07-29 10:15:13 -0500

    feat: validate environment variables

---
+++ src/utils/validate-env.ts
@@ -0,0 +1,29 @@
+import * as z from 'zod';
+
+const envSchema = z.object({
+	NODE_ENV: z.enum(['development', 'production']),
+	PORT: z.string().optional(),
+	HOST: z.string().optional(),
+	CORS_ORIGIN: z.string().optional()
+});
+
+const validateEnv = () => {
+	const parsedEnv = envSchema.safeParse(process.env);
+
+	if (!parsedEnv.success) {
+		console.error('Missing environment variables');
+		parsedEnv.error.issues.map((issue) => {
+			console.error(issue.path[0], issue.message);
+		});
+		process.exit(1);
+	}
+};
+
+export default validateEnv;
+
+declare global {
+	// eslint-disable-next-line @typescript-eslint/no-namespace
+	namespace NodeJS {
+		interface ProcessEnv extends z.infer<typeof envSchema> {}
+	}
+}
